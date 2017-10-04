8c3c461165f8b11116209b7941f3b407e5ff7ac4
Cody Purcell <codypurcell@rocketmail.com>
2017-10-04 15:09:43 -0500

    First part of testing.

---
+++ MathTest.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# Cpurcell - this is to test the complex math function
+#
+import complex as cm
+
+response1 = raw_input("are you using Rectangular notation (R) or Polar (P)")
+response2 = raw_input("First number (x,y) :")
+numberstrings = response2.split(',')
+numberx = float(numberstrings[0][1:])
+numbery = float(numberstrings[1][:-1])
+print numberx
+print numbery
